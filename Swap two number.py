abad658b26ad1e8234cf7b7294a11fe77b0c3bf6
Didhiti Roy <86788901+droy2002@users.noreply.github.com>
2023-02-12 22:48:48 +0530

    Add files via upload

---
+++ Swap two number.py
@@ -0,0 +1,6 @@
+import math
+a=int(input("Enter the 1st number: "))
+b=int(input("Enter the 2nd number: "))
+a,b=b,a
+print("After swapping the 1st number: ",a)
+print("After swapping the 2nd number: ",b)
